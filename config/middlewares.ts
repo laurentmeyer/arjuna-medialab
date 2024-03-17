997388e9bc843cf64148f5502d0e8f0f5744d7ee
lmeyer <laurent.meyer@frontapp.com>
2024-03-17 09:31:26 +0100

    aws s3 config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,37 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::cors",
+  "strapi::body",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "twatter-live.s3.eu-west-3.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "twatter-live.s3.eu-west-3.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
